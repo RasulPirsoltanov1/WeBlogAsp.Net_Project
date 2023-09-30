1c78dbeccbe037307ddfa532b6d50074d2885edc
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-10-01 00:55:06 +0400

    added dasboard page and statistic arguments

---
+++ src/BlogSite.Demo/ViewComponents/Blog/BlogListDashBoard.cs
@@ -0,0 +1,30 @@
+﻿using BlogSite.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Demo.ViewComponents.Blog
+{
+    public class BlogListDashBoard : ViewComponent
+    {
+        IBlogService _blogService;
+        IWriterService _writerService;
+
+        public BlogListDashBoard(IBlogService blogService, IWriterService writerService)
+        {
+            _blogService = blogService;
+            _writerService = writerService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            int writerId = (await _writerService.GetByExpressionAsync(x => x.Mail == User.Identity.Name))[0].Id;
+            if (writerId == 0 || writerId == null)
+            {
+                var blogs1 = await _blogService.values.OrderBy(x => x.CreateDate).Take(3).ToListAsync();
+                return View(blogs1);
+            }
+            var blogs = await _blogService.GetByExpressionAsync(x => x.WriterId == writerId);
+            return View(blogs);
+        }
+    }
+}
---
+++ src/BlogSite.Demo/ViewComponents/Category/CategoryListDasboard.cs
@@ -0,0 +1,22 @@
+﻿using BlogSite.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Demo.ViewComponents.Category
+{
+    public class CategoryListDasboard:ViewComponent
+    {
+        ICategoryService _categoryService;
+
+        public CategoryListDasboard(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories =await _categoryService.values.Include(x=>x.Blogs).ToListAsync();
+            return View(categories);
+        }
+    }
+}
